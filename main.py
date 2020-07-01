ca93c2520484dc6db6286f92e6556fe2d62a5b8f
Arafatsani105 <67681287+Arafatsani105@users.noreply.github.com>
2020-07-01 15:37:08 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests #all this module are required.
+from bs4 import BeautifulSoup
+
+country = input(
+    "Country Name : ")
+scraper_website = requests.get(
+    "https://www.worldometers.info/coronavirus/country/" + country)
+soup = BeautifulSoup(scraper_website.content, 'html.parser')
+
+item = soup.find(id='page-top').get_text()
+cases = soup.find(class_='maincounter-number').get_text()
+recovered = soup.find(
+    class_='maincounter-number', style="color:#8ACA2B ").get_text()
+
+print(item)
+print("Corona Cases:"+cases)
+print("People who recovered  :" + recovered)
